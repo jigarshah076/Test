21ad151d8a8db60e724169c545d7fc23d6ed2668
JIGAR JAIN <jigarshah076@yahoo.com>
2020-09-12 19:25:02 +0530

    Init
    Init

---
+++ CodingTest/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using Contract.BusinessContract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingTest.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        public IMovieService _movieService { get; }
+        public MovieController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        public ActionResult<string> GetAllMovieList()
+        {
+            var data = _movieService.GetAllMovieList();
+            return Newtonsoft.Json.JsonConvert.SerializeObject(data);
+        }
+    }
+}
