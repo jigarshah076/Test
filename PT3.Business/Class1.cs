aab9d8e1a06144f66f5e45e6ef66704007da2b3b
JIGAR JAIN <jigarshah076@yahoo.com>
2020-09-12 19:28:54 +0530

    Backed Done
    Backed Done

---
+++ PT3.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PT3.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ PT3.Business/MovieService.cs
@@ -0,0 +1,21 @@
+﻿using Contract.BusinessContract;
+using Contract.DataAccessContract;
+using Model;
+using System.Collections.Generic;
+
+namespace Business
+{
+    public class MovieService : IMovieService
+    {
+        public IMovieDBClient _movieDBClient { get; }
+        public MovieService(IMovieDBClient movieDBClient)
+        {
+            _movieDBClient = movieDBClient;
+        }
+        
+        public List<Movie> GetAllMovieList()
+        {
+            return _movieDBClient.GetAllMovieList();
+        }
+    }
+}
